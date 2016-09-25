3b055ed6c7dce1630cf23dec77c12f8edd3ddb48
Sven Bacia <sven.bacia@gmail.com>
2016-09-25 19:44:32 +0200

    updates explore endpoints

--- TraktKit/TraktExplore.swift
+++ TraktKit/TraktExplore.swift
@@ -30,28 +30,40 @@ public enum ExploreType {
 
 extension Trakt {
   
-  public func trendingShows(extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<[TrendingShow]> {
-    return resource(for: "/shows/trending", params: parameters(page: page, limit: limit, extended: extended))
+  public func trendingShows(extended: Extended?, page: Int? = nil, limit: Int? = nil, completion: @escaping (Result<[TrendingShow], Error>) -> Void) -> URLSessionTask? {
+    return load(resource: resource(for: "/shows/trending", params: parameters(page: page, limit: limit, extended: extended)),
+                authenticated: false,
+                completion: completion)
   }
   
-  public func popularShows(extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<[Show]> {
-    return resource(for: "/shows/popular", params: parameters(page: page, limit: limit, extended: extended))
+  public func popularShows(extended: Extended?, page: Int? = nil, limit: Int? = nil, completion: @escaping (Result<[Show], Error>) -> Void) -> URLSessionTask? {
+    return load(resource: resource(for: "/shows/popular", params: parameters(page: page, limit: limit, extended: extended)),
+                authenticated: false,
+                completion: completion)
   }
   
-  public func anticipatedShows(extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<[AnticipatedShow]> {
-    return resource(for: "/shows/anticipated", params: parameters(page: page, limit: limit, extended: extended))
+  public func anticipatedShows(extended: Extended?, page: Int? = nil, limit: Int? = nil, completion: @escaping (Result<[AnticipatedShow], Error>) -> Void) -> URLSessionTask? {
+    return load(resource: resource(for: "/shows/anticipated", params: parameters(page: page, limit: limit, extended: extended)),
+                authenticated: false,
+                completion: completion)
   }
   
-  public func playedShows(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<[Show]> {
-    return resource(for: path("/shows/played", with: period), params: parameters(page: page, limit: limit, extended: extended))
+  public func playedShows(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil, completion: @escaping (Result<[Show], Error>) -> Void) -> URLSessionTask? {
+    return load(resource: resource(for: path("/shows/played", with: period), params: parameters(page: page, limit: limit, extended: extended)),
+                authenticated: false,
+                completion: completion)
   }
   
-  public func watchedShows(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<[Show]> {
-    return resource(for: path("/shows/watched", with: period), params: parameters(page: page, limit: limit, extended: extended))
+  public func watchedShows(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil, completion: @escaping (Result<[Show], Error>) -> Void) -> URLSessionTask? {
+    return load(resource: resource(for: path("/shows/watched", with: period), params: parameters(page: page, limit: limit, extended: extended)),
+                authenticated: false,
+                completion: completion)
   }
   
-  public func collectedShows(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<[Show]> {
-    return resource(for: path("/shows/collected", with: period), params: parameters(page: page, limit: limit, extended: extended))
+  public func collectedShows(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil, completion: @escaping (Result<[Show], Error>) -> Void) -> URLSessionTask? {
+    return load(resource: resource(for: path("/shows/collected", with: period), params: parameters(page: page, limit: limit, extended: extended)),
+                authenticated: false,
+                completion: completion)
   }
   
 }
