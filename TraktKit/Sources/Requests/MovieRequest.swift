3bad23363d6b3075bf903c4e0f3cb1e2b13ef0d2
Sven Bacia <sven.bacia@gmail.com>
2017-07-26 18:53:38 +0200

    adds basic movie endpoints

--- TraktKit/Sources/Requests/MovieRequest.swift
+++ TraktKit/Sources/Requests/MovieRequest.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+
+public struct MovieRequest {
+    
+    // MARK: - Properties
+    
+    private let id: Int
+    
+    // MARK: - Init
+    
+    public init(id: Int) {
+        self.id = id
+    }
+    
+    // MARK: - Endpoints
+    
+    public func summary(_ extended: Extended? = nil) -> Resource<Any> {
+        return resource(for: "/movies/\(id)", params: parameters(extended: extended))
+    }
+    
+    public func aliases() -> Resource<Any> {
+        return resource(for: "/movies/\(id)/aliases")
+    }
+    
+    public func releases(`for` country: String? = nil) -> Resource<Any> {
+        var path = "/movies/\(id)/releases"
+        if let country = country {
+            path.append("/\(country)")
+        }
+        return resource(for: path)
+    }
+    
+    public func translations(`for` country: String? = nil) -> Resource<Any> {
+        var path = "/movies/\(id)/translations"
+        if let country = country {
+            path.append("/\(country)")
+        }
+        return resource(for: path)
+    }
+    
+    public func comments(page: Int? = nil, limit: Int? = nil) -> Resource<Any> {
+        return resource(for: "/movies/\(id)/comments", params: parameters(page: page, limit: limit))
+    }
+    
+    public func people(_ extended: Extended? = nil) -> Resource<Any> {
+        return resource(for: "/movies/\(id)/people", params: parameters(extended: extended))
+    }
+    
+    public func ratings() -> Resource<Any> {
+        return resource(for: "/movies/\(id)/rating")
+    }
+    
+    public func related(_ extended: Extended? = nil, page: Int? = nil, limit: Int? = nil) -> Resource<Any> {
+        return resource(for: "/movies/\(id)/related", params: parameters(page: page, limit: limit, extended: extended))
+    }
+    
+    public func stats() -> Resource<Any> {
+        return resource(for: "/movies/\(id)/stats")
+    }
+    
+    public func watching(_ extended: Extended? = nil) -> Resource<Any> {
+        return resource(for: "/movies/\(id)/watching", params: parameters(extended: extended))
+    }
+    
+    public func collectionProgress() -> Resource<Any> {
+        return resource(for: "/shows/\(id)/progress/collection")
+    }
+    
+    public func watchedProgress() -> Resource<Any> {
+        return resource(for: "/shows/\(id)/progress/watched")
+    }
+}
