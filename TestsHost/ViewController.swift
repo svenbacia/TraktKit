8686296d5163c22f0b9c64d544407a0161c35e4f
Sven Bacia <sven.bacia@gmail.com>
2017-12-01 09:34:58 +0100

    adds host application for tests

---
+++ TestsHost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestsHost
+//
+//  Created by Sven Bacia on 01.12.17.
+//  Copyright © 2017 Sven Bacia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
