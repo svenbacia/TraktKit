dc688b66f2c2d84603a8c555a7630757cacbeb4f
Sven Bacia <sven.bacia@gmail.com>
2018-01-14 12:20:01 +0100

    removes MovieResource

--- TraktKit/Sources/Resources/ExploreResource.swift
+++ TraktKit/Sources/Resources/ExploreResource.swift
@@ -25,11 +25,7 @@ public struct ExploreResource {
     public var shows: ShowResource {
         return ShowResource(configuration: configuration)
     }
-    
-//    public var movies: MovieResource {
-//        return MovieResource()
-//    }
-    
+
     // MARK: - TV Shows
     
     public struct ShowResource {
@@ -63,42 +59,5 @@ public struct ExploreResource {
         public func collected(period: Period? = nil, extended: Extended? = nil, page: Int? = nil, limit: Int? = nil) -> Resource<[CollectedShow]> {
             return buildResource(base: configuration.base, path: path("/shows/collected", with: period), params: parameters(page: page, limit: limit, extended: extended))
         }
-    }
-    
-    // MARK: - Movies
-   
-    public struct MovieResource {
-//        public func trending(extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: "/movies/trending", params: parameters(page: page, limit: limit, extended: extended))
-//        }
-//        
-//        public func popular(extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: "/movies/popular", params: parameters(page: page, limit: limit, extended: extended))
-//        }
-//        
-//        public func played(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: path("/movies/played", with: period), params: parameters(page: page, limit: limit, extended: extended))
-//        }
-//        
-//        public func watched(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: path("/movies/watched", with: period), params: parameters(page: page, limit: limit, extended: extended))
-//        }
-//        
-//        public func collected(period: Period? = nil, extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: path("/movies/collected", with: period), params: parameters(page: page, limit: limit, extended: extended))
-//        }
-//        
-//        public func anticipated(extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: "/movies/anticipated", params: parameters(page: page, limit: limit, extended: extended))
-//        }
-//        
-//        public func boxoffice(extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: "/movies/boxoffice", params: parameters(page: page, limit: limit, extended: extended))
-//        }
-//        
-//        public func updates(since date: Date, extended: Extended?, page: Int? = nil, limit: Int? = nil) -> Resource<Data> {
-//            return buildResource(path: "/movies/updates/\(Trakt.DateFormatter.movieUpdateDateFormatter.string(from: date))", params: parameters(page: page, limit: limit, extended: extended))
-//        }
-    }
-    
+    }    
 }
